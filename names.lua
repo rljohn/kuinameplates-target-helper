71e2ee23322c376d2efef84cf47e9523276ecc6d
rljohn <rljohnmoore@gmail.com>
2020-12-24 12:51:15 -0500

    Add files via upload
    Migrate files from perforce to github

---
+++ names.lua
@@ -0,0 +1,112 @@
+ShadowlandsDungeonTargets = {
+	enUS = {
+		"Explosives",
+		"Zolramus Necromancer",
+		"Fungi Stormer",
+		"Mistveil Tender",
+		"Shard of Halkias",
+		"Stoneborn Reaver",
+		"Atal'ai Devoted",
+		"Grand Overseer",
+		"Forsworn Squad-Leader",
+		"Ancient Captain",
+		"Nefarious Darkspeaker",
+	},
+	deDE = {
+		"Sprengstoff",
+		"Nekromant von Zolramus",
+		"Fungistürmer",
+		"Nebelschleierhüterin",
+		"Splitter von Halkias",
+		"Steingeborener Häscher",
+		"Ergebener der Atal'ai",
+		"Oberaufseher",
+		"Schwadronsführer der Verschmähten",
+		"Uralter Hauptmann",
+		"Ruchloser Dunkelsprecher"
+	},
+	esMX = {
+		"Explosivos",
+		"Nigromante de Zolramus",
+		"Agitador de hongos",
+		"Cuidadora Velo de Niebla",
+		"Fragmento de Halkias",
+		"Atracador natopiedra",
+		"Devoto Atal'ai",
+		"Gran sobrestante",
+		"Líder de escuadrón abjurante",
+		"Capitán ancestral",
+		"Hablaoscuro nefario",
+	},
+	frFR = {
+		"Explosifs",
+		"Nécromancien de Zolramus",
+		"Tempétueux fongique",
+		"Soigneuse voile-de-brume",
+		"Eclat d'Halkias",
+		"Saccageuse vive-pierre",
+		"Dévoué atal'ai",
+		"Surveillant suprême",
+		"Chef d'escouade désavoué",
+		"Ancien capitaine",
+		"Sombre orateur néfaste"
+	},
+	itIT = {
+		"Esplosivi",
+		"Necromante di Zolramus",
+		"Assaltatore Fungino",
+		"Guaritrice Velofosco",
+		"Scheggia di Halkias",
+		"Razziatore Litogeno",
+		"Devoto Atal'ai",
+		"Gran Sovrintendente",
+		"Capo-Squadra Indegno",
+		"Capitano Antico",
+		"Oratore Oscuro Nefasto"
+	},
+	ptBR = {
+		"Explosivos",
+		"Necromante Zolramus",
+		"Estrondeador Fungal",
+		"Conservadora do Véu da Névoa",
+		"Estilhaço de Hálkias",
+		"Aniquiladora Litonata",
+		"Devoto Atal'ai",
+		"Grão-feitor",
+		"Líder de Esquadrão Perjuro",
+		"Capitão Ancestral",
+		"Orador Sombrio Nefasto"
+	},
+	ruRU = {
+		"Взрывчатка",
+		"Некромант с \"Золрамуса\"",
+		"Гриб-штурмовик",
+		"Туманная хранительница",
+		"Фрагмент Халкиаса",
+		"Камнерожденный-разоритель",
+		"Верный служитель из племени Атал'ай",
+		"Старший смотритель",
+		"Раскольник-офицер",
+		"Древний капитан",
+		"Пагубный темный проповедник"
+	},
+	koKR = {
+		"폭발물",
+		"조르라무스 강령술사",
+		"버섯 번개술사",
+		"안개장막 숲지기",
+		"할키아스의 조각",
+		"돌숨결 약탈자",
+		"아탈라이 헌신자",
+		"고위 감시자",
+		"이탈자 분대장",
+		"고대의 대장",
+		"사악한 암흑예언자"
+	}
+}
+
+ShadowlandsRaidTargets = {
+	enUS = {
+		"TODO",
+	}
+}
