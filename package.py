bfbd0ad6c2ec39dca98444b95a5ce8bfcfd1bf53
rljohn <rljohnmoore@gmail.com>
2024-05-07 21:58:19 -0400

    Update package.py

--- package.py
+++ package.py
@@ -4,7 +4,7 @@ import os
 from shutil import copytree, ignore_patterns
 from subprocess import check_output
 
-version = "1.4.9"
+version = "1.4.10"
 type = "release"
 
 addon_name = "Kui_Nameplates_TargetHelper"
