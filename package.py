c1658460e20da7a5b2cf417937c8df84a856895d
rljohn <rljohnmoore@gmail.com>
2024-07-28 23:09:09 -0400

    Version bump

--- package.py
+++ package.py
@@ -4,7 +4,7 @@ import os
 from shutil import copytree, ignore_patterns
 from subprocess import check_output
 
-version = "1.5.0"
+version = "1.5.1"
 type = "release"
 
 addon_name = "Kui_Nameplates_TargetHelper"
